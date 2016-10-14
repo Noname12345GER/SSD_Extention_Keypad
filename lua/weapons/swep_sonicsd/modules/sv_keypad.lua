58546f642c453d0581e9d8ca1ff32e14fa625c48
Noname12345GER <steamcommunity.com/id/02861>
2016-10-14 11:08:32 +0200

    Script added.

---
+++ lua/weapons/swep_sonicsd/modules/sv_keypad.lua
@@ -0,0 +1,9 @@
+//Magic. Shield your eyes!
+SWEP:AddFunction(function(self,data)
+	if (data.class=="keypad" or data.class=="keypad_wire") then
+		if data.keydown1 and not data.keydown2 then
+		data.ent:Process(True)
+	elseif data.keydown2 and not data.keydown1 then
+		data.ent:Process(False)
+	end
+end
